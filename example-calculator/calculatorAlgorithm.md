75839802f7092065e2cee2cb57485133c986c348
Konstantinos <kwstas654321@gmail.com>
2021-01-20 10:30:37 +0200

    Front End Buttons
    Front End Completed and Button ara functional

---
+++ example-calculator/calculatorAlgorithm.md
@@ -0,0 +1,23 @@
+Step 1:
+Start Application
+
+Step 2:
+Input number1 
+
+Step 3 
+Input either(sum || substraction || division || multplication || equality)
+
+
+Step 4 
+Input number2 not mandatory if equality
+
+Step 4
+Switch
+    case sum sum(number1, number2)
+    case substraction substract(number1, number2)
+    case multiplication multiply (number1, number2)
+    case division divide(number1, number2)
+    case equality showNumber(number1)
+    
+step 5
+repeat the process
---
+++ example-calculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+
+export class CalculatorComponent implements OnInit {
+  title: string = "Angular Calculator!!!";
+  count: number = 20;
+  finalValue: number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.title);
+  }
+
+  type(value) {
+    this.finalValue = value;
+  }
+  
+}
